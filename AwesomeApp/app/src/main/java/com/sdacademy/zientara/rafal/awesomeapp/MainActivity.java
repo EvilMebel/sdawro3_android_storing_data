06a996b9d76b64ab080c53fa1347049680d947d6
Rafal Zientara <rafal.zientara92@gmail.com>
2017-07-22 12:30:20 +0200

    default settings

--- AwesomeApp/app/src/main/java/com/sdacademy/zientara/rafal/awesomeapp/MainActivity.java
+++ AwesomeApp/app/src/main/java/com/sdacademy/zientara/rafal/awesomeapp/MainActivity.java
@@ -1,11 +1,17 @@
 package com.sdacademy.zientara.rafal.awesomeapp;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.mainActivity_rootView)
+    View rootView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
