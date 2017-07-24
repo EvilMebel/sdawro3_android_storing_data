124d3583ec53d32ed50ae1081a4d770d779db255
Rafal Zientara <rafal.zientara92@gmail.com>
2017-07-24 23:49:26 +0200

    fragment prefs

--- AwesomeApp/app/src/main/java/com/sdacademy/zientara/rafal/awesomeapp/MainActivity.java
+++ AwesomeApp/app/src/main/java/com/sdacademy/zientara/rafal/awesomeapp/MainActivity.java
@@ -1,22 +1,51 @@
 package com.sdacademy.zientara.rafal.awesomeapp;
 
 import android.graphics.Color;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+
+import com.sdacademy.zientara.rafal.awesomeapp.fragments.SettingsFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-
-    @BindView(R.id.mainActivity_rootView)
-    View rootView;
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        openSettingsFragment();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            openSettingsFragment();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void openSettingsFragment() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.mainActivity_fragmentContainer, new SettingsFragment());
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
---
+++ AwesomeApp/app/src/main/java/com/sdacademy/zientara/rafal/awesomeapp/fragments/SettingsFragment.java
@@ -0,0 +1,23 @@
+package com.sdacademy.zientara.rafal.awesomeapp.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Evil on 24.07.2017.
+ */
+
+public class SettingsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //// TODO: 24.07.2017
+        
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
