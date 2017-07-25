629f6ad478fc96831f4140976dcb9cce65c03d9c
Rafal Zientara <rafal.zientara92@gmail.com>
2017-07-25 17:39:02 +0200

    working fake settings

--- AwesomeApp/app/src/main/java/com/sdacademy/zientara/rafal/awesomeapp/MainActivity.java
+++ AwesomeApp/app/src/main/java/com/sdacademy/zientara/rafal/awesomeapp/MainActivity.java
@@ -1,26 +1,23 @@
 package com.sdacademy.zientara.rafal.awesomeapp;
 
-import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.sdacademy.zientara.rafal.awesomeapp.fragments.SettingsFragment;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
 public class MainActivity extends AppCompatActivity {
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         openSettingsFragment();
--- AwesomeApp/app/src/main/java/com/sdacademy/zientara/rafal/awesomeapp/fragments/SettingsFragment.java
+++ AwesomeApp/app/src/main/java/com/sdacademy/zientara/rafal/awesomeapp/fragments/SettingsFragment.java
@@ -3,9 +3,14 @@ package com.sdacademy.zientara.rafal.awesomeapp.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.preference.PreferenceFragmentCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.sdacademy.zientara.rafal.awesomeapp.R;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by Evil on 24.07.2017.
@@ -16,8 +21,8 @@ public class SettingsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //// TODO: 24.07.2017
-        
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 }
