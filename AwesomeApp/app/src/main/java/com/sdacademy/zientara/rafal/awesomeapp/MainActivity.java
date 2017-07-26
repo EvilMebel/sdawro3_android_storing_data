9ba26a7ac247ffba86b6a7421d398ddd074c03c6
Rafal Zientara <rafal.zientara92@gmail.com>
2017-07-26 18:48:36 +0200

    go to external or root directory from menu

--- AwesomeApp/app/src/main/java/com/sdacademy/zientara/rafal/awesomeapp/MainActivity.java
+++ AwesomeApp/app/src/main/java/com/sdacademy/zientara/rafal/awesomeapp/MainActivity.java
@@ -1,12 +1,16 @@
 package com.sdacademy.zientara.rafal.awesomeapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.sdacademy.zientara.rafal.awesomeapp.fragments.ExplorerFragment;
 import com.sdacademy.zientara.rafal.awesomeapp.fragments.SettingsFragment;
@@ -14,6 +18,8 @@ import com.sdacademy.zientara.rafal.awesomeapp.fragments.SettingsFragment;
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity implements ExplorerFragment.ExploratorInteractionListener {
+
+    private static final int EXTERNAL_STORAGE_REQUEST_CODE = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,12 +49,37 @@ public class MainActivity extends AppCompatActivity implements ExplorerFragment.
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            openSettingsFragment();
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                openSettingsFragment();
+                return true;
+
+            case R.id.action_goto_external:
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, EXTERNAL_STORAGE_REQUEST_CODE);
+                return true;
+
+
+            case R.id.action_goto_root:
+                openExplorerFragment(Environment.getRootDirectory().getPath(), false);
+                return true;
+
+
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case EXTERNAL_STORAGE_REQUEST_CODE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    openExplorerFragment(Environment.getExternalStorageDirectory().getPath(), false);
+                else
+                    Toast.makeText(this, ":(", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
     }
 
     private void openSettingsFragment() {
