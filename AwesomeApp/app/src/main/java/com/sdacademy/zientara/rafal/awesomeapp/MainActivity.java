e00dba28c3a940d77ead3f13f41586831c9d5340
Rafal Zientara <rafal.zientara92@gmail.com>
2017-07-27 19:16:09 +0200

    empty base project

--- AwesomeApp/app/src/main/java/com/sdacademy/zientara/rafal/awesomeapp/MainActivity.java
+++ AwesomeApp/app/src/main/java/com/sdacademy/zientara/rafal/awesomeapp/MainActivity.java
@@ -1,26 +1,14 @@
 package com.sdacademy.zientara.rafal.awesomeapp;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.Environment;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-import com.sdacademy.zientara.rafal.awesomeapp.fragments.ExplorerFragment;
-import com.sdacademy.zientara.rafal.awesomeapp.fragments.OpenFileFragment;
 import com.sdacademy.zientara.rafal.awesomeapp.fragments.SettingsFragment;
 
-import java.io.File;
-
-public class MainActivity extends BaseActivity implements ExplorerFragment.ExploratorInteractionListener,
-        OpenFileFragment.InteractionListener{
+public class MainActivity extends BaseActivity {
     private static final int EXTERNAL_STORAGE_REQUEST_CODE = 1500;
     private String currentPath;
 
@@ -30,13 +18,9 @@ public class MainActivity extends BaseActivity implements ExplorerFragment.Explo
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        openExplorerFragment(Environment.getRootDirectory().getPath(), false);
+        openSettingsFragment();
     }
 
-    private void openExplorerFragment(String path, boolean canGoBack) {
-        Fragment fragment = ExplorerFragment.newInstance(path);
-        openFragment(fragment, canGoBack);
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -50,63 +34,12 @@ public class MainActivity extends BaseActivity implements ExplorerFragment.Explo
             case R.id.action_settings:
                 openSettingsFragment();
                 return true;
-
-            case R.id.action_goto_external:
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, EXTERNAL_STORAGE_REQUEST_CODE);
-                return true;
-
-            case R.id.action_goto_root:
-                openExplorerFragment(Environment.getRootDirectory().getPath(), false);
-                return true;
-
-            case R.id.action_add_file:
-                openFragment(OpenFileFragment.newInstance(currentPath + File.separator + "filename.txt", true), true);
-                return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case EXTERNAL_STORAGE_REQUEST_CODE: {
-                if (grantResults.length == 2
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED
-                        && grantResults[1] == PackageManager.PERMISSION_GRANTED)
-                    openExplorerFragment(Environment.getExternalStorageDirectory().getPath(), false);
-                else
-                    Toast.makeText(this, ":(", Toast.LENGTH_SHORT).show();
-                return;
-            }
-        }
-    }
 
     private void openSettingsFragment() {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.add(R.id.mainActivity_fragmentContainer, new SettingsFragment());
-        transaction.addToBackStack(null);
-        transaction.commit();
-    }
-
-    @Override
-    public void onDirectoryClicked(String newPath) {
-        openExplorerFragment(newPath, true);
-        currentPath = newPath;
-    }
-
-    @Override
-    public void onFileClicked(String filePath) {
-        openFragment(OpenFileFragment.newInstance(filePath), true);
-    }
-
-    @Override
-    public void onBackClicked() {
-        //// TODO: 26.07.2017 get fragment and get info
-        getSupportFragmentManager().popBackStack();
-    }
-
-    @Override
-    public void doNothing() {
-        //lol
+        openFragment(new SettingsFragment(), false);
     }
 }
